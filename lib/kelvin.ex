b14db6ab618ea30147d35376707b0e9a1148a068
Michael Davis <mcarsondavis@gmail.com>
2021-05-28 15:43:57 -0500

    add a function for getting stream position

--- lib/kelvin.ex
+++ lib/kelvin.ex
@@ -2,4 +2,16 @@ defmodule Kelvin do
   @moduledoc """
   GenStage/Broadway producers for Extreme
   """
+
+  @doc """
+  Determines the stream position of a subscription event from Extreme
+
+  This function can be used by a GenStage consumer to a linear subscription in
+  order to persist stream position.
+  """
+  # coveralls-ignore-start
+  def stream_position(%{link: %{event_number: n}}) when is_number(n), do: n
+  def stream_position(%{event: %{event_number: n}}) when is_number(n), do: n
+
+  # coveralls-ignore-stop
 end
