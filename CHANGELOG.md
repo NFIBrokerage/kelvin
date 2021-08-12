058286bd9c13652a36f0ff95ed864febc32f7ac4
Michael Davis <mcarsondavis@gmail.com>
2021-08-12 09:18:51 -0500

    queue up events from extreme in the InOrder producer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,15 @@ The format is based on [Keep a
 Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to
 [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 0.4.1 - 2021-08-12
+
+### Changed
+
+- Changed the InOrderSubscription internals to buffer events from the
+  extreme subscription in a queue when there is no demand
+    - this unlocks the ability for this producer to emit multiple events
+      at once
+
 ## 0.4.0 - 2021-06-10
 
 ### Added
--- test/fixtures/my_linear_consumer.ex
+++ test/fixtures/my_linear_consumer.ex
@@ -13,15 +13,20 @@ defmodule MyInOrderConsumer do
   def init(opts) do
     test_proc = Keyword.fetch!(opts, :test_proc)
     subscribe_to = Keyword.fetch!(opts, :subscribe_to)
+    sleep_time = Keyword.fetch!(opts, :sleep_time)
 
-    {:consumer, test_proc, subscribe_to: subscribe_to}
+    {:consumer, {test_proc, sleep_time}, subscribe_to: subscribe_to}
   end
 
   @impl GenStage
-  def handle_events(events, _from, test_proc) do
+  def handle_events(events, _from, {test_proc, sleep_time}) do
+    if sleep_time |> is_integer() do
+      Process.sleep(sleep_time)
+    end
+
     events = Enum.map(events, fn {_producer, event} -> event end)
     send(test_proc, {:events, events})
 
-    {:noreply, [], test_proc}
+    {:noreply, [], {test_proc, sleep_time}}
   end
 end
